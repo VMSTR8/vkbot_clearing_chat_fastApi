361914962747b82e8368e2fb86fa526ad8d9967d
Maxim Vinokurov <vmstr8@yandex.ru>
2021-12-26 17:04:13 +0300

    Настройки проекта

---
+++ settings.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+CONFIRMATION_TOKEN = os.environ.get('CONFIRMATION_TOKEN')
+
+OPEN_GROUP_TOKEN = os.environ.get('OPEN_GROUP_TOKEN')
+CLOSED_GROUP_TOKEN = os.environ.get('CLOSED_GROUP_TOKEN')
+
+ADMIN_ID = os.environ.get('ADMIN_ID')
+GROUP_ID = os.environ.get('GROUP_ID')
